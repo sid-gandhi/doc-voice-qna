d43d5c4f15da1645cd38ce7a2a5c8905bd3dbe07
Siddharth Gandhi <siddharthgandhi005@gmail.com>
2024-12-22 23:28:10 +0530

    make the chat histroy scrollable

--- src/components/Conversation.tsx
+++ src/components/Conversation.tsx
@@ -56,7 +56,7 @@ type ChatHistoryProps = { messages: ConversationMessage[] };
 const ChatHistory = ({ messages }: ChatHistoryProps) => {
   return (
     <div>
-      <ScrollArea>
+      <ScrollArea className="h-[600px] p-4">
         <div className="max-w-2xl mx-auto p-4 space-y-4">
           {messages.map((msg, idx) => (
             <ChatBubble key={idx} {...msg} />
